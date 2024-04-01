47ac696999a48f6af7357acd1baf8d6396fc54dc
ctrimble6 <ctrimble6@gatech.edu>
2024-04-01 10:17:28 -0400

    Update bidomainCLT.py
    -Added parameters to unify notation
    -built initialization for v_tm
    -cleaned up notation for test script

--- bidomainCLT.py
+++ bidomainCLT.py
@@ -9,14 +9,29 @@ class BidomainSolver:
         self.parallelization = parallelization
         #self.ionModel = ionModel if ionModel else IonModel() # IonModel class to be implemented
         self.time = time
-        self.timestep_size = timestep_size
+        self.dt = timestep_size
         self.shape = shape
         self.pacing = pacing
         self.conductivity = conductivity
-        self.grid_step = 0.01
+        self.dx = 0.01
+        self.t = 0.0
 
-## Crank-Nicolson initialization
-    def cn_init_1d(self,S_input,BC,het_input='homogenous'):
+    def initialize_v(self,IC='uniform'):
+        self.N = int(len(self.shape) / self.dx)
+        self.v_tm = np.ones(self.N)
+
+        if IC == 'uniform':
+            pass
+        elif IC == 'step':
+            self.v_tm[int(self.N/2):] = 2*self.v_tm[int(self.N/2):]
+        elif IC == 'sine':
+            self.v_tm = 1 + 0.5*np.sin(np.pi*np.linspace(0, self.L, self.N))
+        else:
+            raise ValueError('Initial Condition not supported')
+
+
+    ## Crank-Nicolson initialization
+    def cn_init_1d(self,S_input,BC,het_input='homogeneous'):
         # Initialize the Crank-Nicolson method for a 1D problem
         N = self.N
 
@@ -28,7 +43,7 @@ class BidomainSolver:
             S = 1 + 0.5*np.sin(np.pi*np.linspace(0, self.L, N))
         elif het_input == 'random':
             S = S_input*np.random.rand(N)
-        elif het_input == 'homogenous':
+        elif het_input == 'homogeneous':
             S = S_input*np.ones(N)
         else:
             raise ValueError('Heterogeneity not supported')
@@ -69,7 +84,7 @@ class BidomainSolver:
         
         self.A,self.B = A,B
 
-    def thomas_method(self, d):
+    def thomas_method(self, Bv_tm):
         # Check if the matrix is tridiagonal
         if not np.all(self.A == np.diag(np.diag(self.A)) + 
                       np.diag(np.diag(self.A, k=1), k=1) + 
@@ -78,6 +93,7 @@ class BidomainSolver:
         
         n = len(self.A)
         x = np.zeros(n)
+        rhs = Bv_tm
 
         matrix = self.A.copy() # Copy the matrix to avoid modifying the original
         
@@ -85,28 +101,30 @@ class BidomainSolver:
         for i in range(1, n):
             m = matrix[i][i-1] / matrix[i-1][i-1]
             matrix[i] -= m * matrix[i-1]
-            d[i] -= m * d[i-1]
+            rhs[i] -= m * rhs[i-1]
         
         # Perform backsubstitution
-        x[n-1] = d[n-1] / matrix[n-1][n-1]
+        x[n-1] = rhs[n-1] / matrix[n-1][n-1]
         for i in range(n-2, -1, -1):
-            x[i] = (d[i] - matrix[i][i+1] * x[i+1]) / matrix[i][i]
+            x[i] = ([i] - matrix[i][i+1] * x[i+1]) / matrix[i][i]
         
         del matrix
+        self.v_tm = np.copy(x)
 
-        return x
-
-    def cn_solve(self,v_tm):
+    def cn_solve(self):
         ## dv_tm/dt = S * d^2v_tm/dx^2 - ion_current(v_tm,)/C_m
         ## v_tm(x,t+dt) = v_tm(x,t) + dt * (S * d^2v_tm/dx^2 - ion_current(v_tm,)/C_m)
         ## A * v_tp = B * v_tm
         ## A is a tri-diagonal matrix with 1 + 2*alpha on the diagonal and -alpha on the off-diagonals
         ## B is a tri-diagonal matrix with 1 - 2*alpha on the diagonal and alpha on the off-diagonals
 
-        b = self.B @ v_tm
-        v_tp = self.thomas_method(b) # v_tp is the predicted solution at the next time step
 
-        return v_tp
+        while self.t < self.time:
+            rhs = self.B @ self.v_tm # This will also include the ion current term eventually
+            self.thomas_method(rhs)
+            self.t += self.dt
+
+
 
     def apply_pacing(self):
         # This will eventually apply a pacing protocol
